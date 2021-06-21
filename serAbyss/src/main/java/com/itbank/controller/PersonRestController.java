b972ba6f4c8da48f963721e61aa8eb2ba30e6205
about2000s <about2000s@naver.com>
2021-06-21 15:22:50 +0900

    [update]sqlmap-customer외 고객응대 비동기 통신 구현중

--- serAbyss/src/main/java/com/itbank/controller/PersonRestController.java
+++ serAbyss/src/main/java/com/itbank/controller/PersonRestController.java
@@ -5,14 +5,12 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -28,8 +26,6 @@ public class PersonRestController {
 	@Autowired private CustomerService cs; 
 	
 	private ObjectMapper mapper = new ObjectMapper();
-	
-	
 	
 	@GetMapping(value = "/compSearch/{keyword}", produces = "application/json; charset=utf-8")
 	public String compSearch(@PathVariable String keyword) throws JsonProcessingException {
